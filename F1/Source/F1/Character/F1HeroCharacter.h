5ec676b273e19125d6ae9433075a6f6e9287dc6c
Hyoseon <wink2090@naver.com>
2025-09-09 00:18:49 +0900

    feat: create hero,jungle,monster cpp class

---
+++ F1/Source/F1/Character/F1HeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/F1CharacterBase.h"
+#include "F1HeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F1_API AF1HeroCharacter : public AF1CharacterBase
+{
+	GENERATED_BODY()
+	
+};
