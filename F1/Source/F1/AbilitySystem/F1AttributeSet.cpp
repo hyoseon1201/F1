a4f323541f58a607de4c93637e7c369b31c8340b
Hyoseon <wink2090@naver.com>
2025-09-12 15:34:19 +0900

    feat: add health, mana to attributeset

--- F1/Source/F1/AbilitySystem/F1AttributeSet.cpp
+++ F1/Source/F1/AbilitySystem/F1AttributeSet.cpp
@@ -2,4 +2,39 @@
 
 
 #include "AbilitySystem/F1AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UF1AttributeSet::UF1AttributeSet()
+{
+}
+
+void UF1AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UF1AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Health, OldHealth);
+}
+
+void UF1AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& MaxOldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxHealth, MaxOldHealth);
+}
+
+void UF1AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Mana, OldMana);
+}
+
+void UF1AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& MaxOldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxMana, MaxOldMana);
+}
