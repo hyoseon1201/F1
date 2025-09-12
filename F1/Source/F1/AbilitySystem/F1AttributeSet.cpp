c8ea6fc3c7b423ddced9f26b5447f96fd3891b76
Hyoseon <wink2090@naver.com>
2025-09-13 00:19:45 +0900

    feat: add AttributeSet member

--- F1/Source/F1/AbilitySystem/F1AttributeSet.cpp
+++ F1/Source/F1/AbilitySystem/F1AttributeSet.cpp
@@ -1,35 +1,109 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AbilitySystem/F1AttributeSet.h"
 #include "Net/UnrealNetwork.h"
 
 UF1AttributeSet::UF1AttributeSet()
 {
+	// 기본 생존 능력치 초기화
 	InitHealth(100.f);
 	InitMaxHealth(100.f);
+	InitHealthRegeneration(1.0f);
 	InitMana(50.f);
 	InitMaxMana(50.f);
+	InitManaRegeneration(1.0f);
+
+	// 공격 능력치 초기화
+	InitAttackDamage(60.f);
+	InitAttackSpeed(0.625f);  // LOL 기본 공속
+	InitAbilityPower(0.f);
+	InitCriticalStrikeChance(0.f);
+	InitCriticalStrikeDamage(200.f);  // 치명타 기본 200%
+
+	// 방어 능력치 초기화
+	InitArmor(30.f);
+	InitMagicResistance(30.f);
+
+	// 이동 및 유틸리티 초기화
+	InitMovementSpeed(325.f);  // LOL 기본 이속
+	InitAbilityHaste(0.f);
+
+	// 관통력 초기화
+	InitArmorPenetration(0.f);
+	InitMagicPenetration(0.f);
+
+	// 흡혈 초기화
+	InitLifeSteal(0.f);
+	InitOmnivamp(0.f);
+
+	// 저항력 초기화
+	InitTenacity(0.f);
+	InitSlowResistance(0.f);
+
+	// 사거리 초기화
+	InitAttackRange(125.f);  // 근접 캐릭터 기본 사거리
 }
 
 void UF1AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	// 기본 생존 능력치
 	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Health, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, HealthRegeneration, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, ManaRegeneration, COND_None, REPNOTIFY_Always);
+
+	// 공격 능력치
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, AttackDamage, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, AttackSpeed, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, AbilityPower, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, CriticalStrikeChance, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, CriticalStrikeDamage, COND_None, REPNOTIFY_Always);
+
+	// 방어 능력치
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Armor, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MagicResistance, COND_None, REPNOTIFY_Always);
+
+	// 이동 및 유틸리티
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MovementSpeed, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, AbilityHaste, COND_None, REPNOTIFY_Always);
+
+	// 관통력
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, ArmorPenetration, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, MagicPenetration, COND_None, REPNOTIFY_Always);
+
+	// 흡혈
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, LifeSteal, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Omnivamp, COND_None, REPNOTIFY_Always);
+
+	// 저항력
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, Tenacity, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, SlowResistance, COND_None, REPNOTIFY_Always);
+
+	// 사거리
+	DOREPLIFETIME_CONDITION_NOTIFY(UF1AttributeSet, AttackRange, COND_None, REPNOTIFY_Always);
 }
+
+// ===========================================
+// OnRep 함수들 구현
+// ===========================================
 
 void UF1AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Health, OldHealth);
 }
 
-void UF1AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& MaxOldHealth) const
+void UF1AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxHealth, MaxOldHealth);
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UF1AttributeSet::OnRep_HealthRegeneration(const FGameplayAttributeData& OldHealthRegeneration) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, HealthRegeneration, OldHealthRegeneration);
 }
 
 void UF1AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
@@ -37,7 +111,92 @@ void UF1AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Mana, OldMana);
 }
 
-void UF1AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& MaxOldMana) const
+void UF1AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxMana, MaxOldMana);
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MaxMana, OldMaxMana);
+}
+
+void UF1AttributeSet::OnRep_ManaRegeneration(const FGameplayAttributeData& OldManaRegeneration) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, ManaRegeneration, OldManaRegeneration);
+}
+
+void UF1AttributeSet::OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, AttackDamage, OldAttackDamage);
+}
+
+void UF1AttributeSet::OnRep_AttackSpeed(const FGameplayAttributeData& OldAttackSpeed) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, AttackSpeed, OldAttackSpeed);
+}
+
+void UF1AttributeSet::OnRep_AbilityPower(const FGameplayAttributeData& OldAbilityPower) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, AbilityPower, OldAbilityPower);
+}
+
+void UF1AttributeSet::OnRep_CriticalStrikeChance(const FGameplayAttributeData& OldCriticalStrikeChance) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, CriticalStrikeChance, OldCriticalStrikeChance);
+}
+
+void UF1AttributeSet::OnRep_CriticalStrikeDamage(const FGameplayAttributeData& OldCriticalStrikeDamage) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, CriticalStrikeDamage, OldCriticalStrikeDamage);
+}
+
+void UF1AttributeSet::OnRep_Armor(const FGameplayAttributeData& OldArmor) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Armor, OldArmor);
+}
+
+void UF1AttributeSet::OnRep_MagicResistance(const FGameplayAttributeData& OldMagicResistance) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MagicResistance, OldMagicResistance);
+}
+
+void UF1AttributeSet::OnRep_MovementSpeed(const FGameplayAttributeData& OldMovementSpeed) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MovementSpeed, OldMovementSpeed);
+}
+
+void UF1AttributeSet::OnRep_AbilityHaste(const FGameplayAttributeData& OldAbilityHaste) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, AbilityHaste, OldAbilityHaste);
+}
+
+void UF1AttributeSet::OnRep_ArmorPenetration(const FGameplayAttributeData& OldArmorPenetration) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, ArmorPenetration, OldArmorPenetration);
+}
+
+void UF1AttributeSet::OnRep_MagicPenetration(const FGameplayAttributeData& OldMagicPenetration) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, MagicPenetration, OldMagicPenetration);
+}
+
+void UF1AttributeSet::OnRep_LifeSteal(const FGameplayAttributeData& OldLifeSteal) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, LifeSteal, OldLifeSteal);
+}
+
+void UF1AttributeSet::OnRep_Omnivamp(const FGameplayAttributeData& OldOmnivamp) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Omnivamp, OldOmnivamp);
+}
+
+void UF1AttributeSet::OnRep_Tenacity(const FGameplayAttributeData& OldTenacity) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, Tenacity, OldTenacity);
+}
+
+void UF1AttributeSet::OnRep_SlowResistance(const FGameplayAttributeData& OldSlowResistance) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, SlowResistance, OldSlowResistance);
+}
+
+void UF1AttributeSet::OnRep_AttackRange(const FGameplayAttributeData& OldAttackRange) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UF1AttributeSet, AttackRange, OldAttackRange);
 }
--- F1/Source/F1/AbilitySystem/F1AttributeSet.h
+++ F1/Source/F1/AbilitySystem/F1AttributeSet.h
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -8,24 +7,26 @@
 #include "F1AttributeSet.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
- 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
- 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
- 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
- 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
- * 
+ *
  */
 UCLASS()
 class F1_API UF1AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
 public:
 	UF1AttributeSet();
-
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	// ===========================================
+	// 기본 생존 능력치 (Vital Stats)
+	// ===========================================
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UF1AttributeSet, Health);
@@ -33,6 +34,10 @@ public:
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
 	FGameplayAttributeData MaxHealth;
 	ATTRIBUTE_ACCESSORS(UF1AttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthRegeneration, Category = "Vital Attributes")
+	FGameplayAttributeData HealthRegeneration;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, HealthRegeneration);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
@@ -42,15 +47,161 @@ public:
 	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS(UF1AttributeSet, MaxMana);
 
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ManaRegeneration, Category = "Vital Attributes")
+	FGameplayAttributeData ManaRegeneration;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, ManaRegeneration);
+
+	// ===========================================
+	// 공격 능력치 (Offensive Stats)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AttackDamage, Category = "Offensive Attributes")
+	FGameplayAttributeData AttackDamage;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, AttackDamage);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AttackSpeed, Category = "Offensive Attributes")
+	FGameplayAttributeData AttackSpeed;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, AttackSpeed);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AbilityPower, Category = "Offensive Attributes")
+	FGameplayAttributeData AbilityPower;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, AbilityPower);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CriticalStrikeChance, Category = "Offensive Attributes")
+	FGameplayAttributeData CriticalStrikeChance;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, CriticalStrikeChance);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CriticalStrikeDamage, Category = "Offensive Attributes")
+	FGameplayAttributeData CriticalStrikeDamage;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, CriticalStrikeDamage);
+
+	// ===========================================
+	// 방어 능력치 (Defensive Stats)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Armor, Category = "Defensive Attributes")
+	FGameplayAttributeData Armor;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, Armor);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MagicResistance, Category = "Defensive Attributes")
+	FGameplayAttributeData MagicResistance;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, MagicResistance);
+
+	// ===========================================
+	// 이동 및 유틸리티 (Movement & Utility)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MovementSpeed, Category = "Movement Attributes")
+	FGameplayAttributeData MovementSpeed;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, MovementSpeed);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AbilityHaste, Category = "Utility Attributes")
+	FGameplayAttributeData AbilityHaste;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, AbilityHaste);
+
+	// ===========================================
+	// 관통력 (Penetration)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ArmorPenetration, Category = "Penetration Attributes")
+	FGameplayAttributeData ArmorPenetration;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, ArmorPenetration);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MagicPenetration, Category = "Penetration Attributes")
+	FGameplayAttributeData MagicPenetration;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, MagicPenetration);
+
+	// ===========================================
+	// 흡혈 (Life Steal & Spell Vamp)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_LifeSteal, Category = "Sustain Attributes")
+	FGameplayAttributeData LifeSteal;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, LifeSteal);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Omnivamp, Category = "Sustain Attributes")
+	FGameplayAttributeData Omnivamp;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, Omnivamp);
+
+	// ===========================================
+	// 저항력 (Resistances)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Tenacity, Category = "Resistance Attributes")
+	FGameplayAttributeData Tenacity;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, Tenacity);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_SlowResistance, Category = "Resistance Attributes")
+	FGameplayAttributeData SlowResistance;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, SlowResistance);
+
+	// ===========================================
+	// 사거리 (Range)
+	// ===========================================
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AttackRange, Category = "Range Attributes")
+	FGameplayAttributeData AttackRange;
+	ATTRIBUTE_ACCESSORS(UF1AttributeSet, AttackRange);
+
+	// ===========================================
+	// OnRep 함수들
+	// ===========================================
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 
 	UFUNCTION()
-	void OnRep_MaxHealth(const FGameplayAttributeData& MaxOldHealth) const;
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_HealthRegeneration(const FGameplayAttributeData& OldHealthRegeneration) const;
 
 	UFUNCTION()
 	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
 
 	UFUNCTION()
-	void OnRep_MaxMana(const FGameplayAttributeData& MaxOldMana) const;
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+	UFUNCTION()
+	void OnRep_ManaRegeneration(const FGameplayAttributeData& OldManaRegeneration) const;
+
+	UFUNCTION()
+	void OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage) const;
+
+	UFUNCTION()
+	void OnRep_AttackSpeed(const FGameplayAttributeData& OldAttackSpeed) const;
+
+	UFUNCTION()
+	void OnRep_AbilityPower(const FGameplayAttributeData& OldAbilityPower) const;
+
+	UFUNCTION()
+	void OnRep_CriticalStrikeChance(const FGameplayAttributeData& OldCriticalStrikeChance) const;
+
+	UFUNCTION()
+	void OnRep_CriticalStrikeDamage(const FGameplayAttributeData& OldCriticalStrikeDamage) const;
+
+	UFUNCTION()
+	void OnRep_Armor(const FGameplayAttributeData& OldArmor) const;
+
+	UFUNCTION()
+	void OnRep_MagicResistance(const FGameplayAttributeData& OldMagicResistance) const;
+
+	UFUNCTION()
+	void OnRep_MovementSpeed(const FGameplayAttributeData& OldMovementSpeed) const;
+
+	UFUNCTION()
+	void OnRep_AbilityHaste(const FGameplayAttributeData& OldAbilityHaste) const;
+
+	UFUNCTION()
+	void OnRep_ArmorPenetration(const FGameplayAttributeData& OldArmorPenetration) const;
+
+	UFUNCTION()
+	void OnRep_MagicPenetration(const FGameplayAttributeData& OldMagicPenetration) const;
+
+	UFUNCTION()
+	void OnRep_LifeSteal(const FGameplayAttributeData& OldLifeSteal) const;
+
+	UFUNCTION()
+	void OnRep_Omnivamp(const FGameplayAttributeData& OldOmnivamp) const;
+
+	UFUNCTION()
+	void OnRep_Tenacity(const FGameplayAttributeData& OldTenacity) const;
+
+	UFUNCTION()
+	void OnRep_SlowResistance(const FGameplayAttributeData& OldSlowResistance) const;
+
+	UFUNCTION()
+	void OnRep_AttackRange(const FGameplayAttributeData& OldAttackRange) const;
 };
