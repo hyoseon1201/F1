7b3de55a44695bc22f3aa1885308991f756c45da
hyoseon <wink2090@naver.com>
2025-11-12 21:09:38 +0900

    feat: EffectContext 적용

--- F1/Source/F1/AbilitySystem/GameplayAbility/F1ProjectileSpell.cpp
+++ F1/Source/F1/AbilitySystem/GameplayAbility/F1ProjectileSpell.cpp
@@ -40,10 +40,18 @@ void UF1ProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation
 
 	Projectile->SetOwner(GetAvatarActorFromActorInfo());
 	const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-	const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(
-		DamageEffectClass,
-		GetAbilityLevel(),
-		SourceASC->MakeEffectContext());
+	
+	FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+	EffectContextHandle.SetAbility(this);
+	EffectContextHandle.AddSourceObject(Projectile);
+	TArray<TWeakObjectPtr<AActor>> Actors;
+	Actors.Add(Projectile);
+	EffectContextHandle.AddActors(Actors);
+	FHitResult HitResult;
+	HitResult.Location = ProjectileTargetLocation;
+	EffectContextHandle.AddHitResult(HitResult);
+
+	const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
 
 	FF1GameplayTags GameplayTags = FF1GameplayTags::Get();
 	const float ScaledDamage = Damage.GetValueAtLevel(GetAbilityLevel());
